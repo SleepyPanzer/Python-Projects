9dc92d6f44b3663f2325eeb54da888e2d6c6b5a1
SleepyPanzer <shaheer.syed2002@gmail.com>
2024-09-12 16:03:20 +0500

    shooping cart

---
+++ Py-Projects/shopping_cart.py
@@ -0,0 +1,22 @@
+foods=[]
+price=[]
+total=0
+while True:
+    ans=input("enter the foos that you want or press Q to quit :")
+    if ans.lower()=="q":
+        break
+    else:
+        prices=int(input("enter the price of the food :"))
+        foods.append(ans)
+        price.append(prices)
+        
+      
+print("##########cart###############") 
+for food in foods:
+    print(food,end=" ")
+for p in price :
+    total+=prices
+    
+    
+print(f"your total bill is : ${total}")
+    
